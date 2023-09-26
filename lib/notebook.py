24795f7b9188904c832681b1a0d096ec68acc637
Magyar-Hunor Tamas <magyar-hunor.tamas@makers.tech>
2023-09-26 11:07:27 +0100

    Add new Notebook test, implement and pass

--- lib/notebook.py
+++ lib/notebook.py
@@ -1,3 +1,6 @@
 class Notebook():
     def __init__(self):
         self.contents = []
+
+    def add(self, entry):
+        self.contents.append(entry)
--- tests/test_notebook.py
+++ tests/test_notebook.py
@@ -7,3 +7,11 @@ def test_instantiates_with_contents():
     actual = notebook.contents
 
     assert actual == expected
+
+def test_add_one_note():
+    notebook = Notebook()
+
+    notebook.add('entry 1')
+
+    assert notebook.contents[0] == 'entry 1'
+    assert 'entry 1' in notebook.contents
